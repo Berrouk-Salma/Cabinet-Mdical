357fa1467a5ce48f75ddf4d460d0ef8e75ed18d4
Berrouk-Salma <salmaberrouk4@gmail>
2025-02-05 12:23:45 +0100

    edit router

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,7 +1,23 @@
 <?php
+namespace App\Core;
+
 class Router {
+    private $routes = [];
+
+    public function addRoute($url, $controller, $action) {
+        $this->routes[$url] = ['controller' => $controller, 'action' => $action];
+    }
+
     public function dispatch() {
         $url = $_SERVER['REQUEST_URI'];
-        // Example: Map "/patients" to PatientsController@index
+        if (array_key_exists($url, $this->routes)) {
+            $controller = $this->routes[$url]['controller'];
+            $action = $this->routes[$url]['action'];
+            $controllerInstance = new $controller();
+            $controllerInstance->$action();
+        } else {
+            echo "404 - Page not found!";
+        }
     }
 }
+?>
